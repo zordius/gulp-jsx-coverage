c0792436eefe0526601240f2afe56b340ea3e0ef
Zordius Chen <zordius@yahoo-inc.com>
2014-12-30 16:18:27 +0800

    refine

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 
 var gulp = require('gulp'),
     istanbul = require('istanbul'),
+    mocha = require('gulp-mocha'),
 
 // Never use node-jsx or other transform in your testing code!
 initIstanbulHookHack = function (options) {
     var Module = require('module'),
-        instrumenter = new istanbul.Instrumenter(configs.test_coverage.istanbul);
+        instrumenter = new istanbul.Instrumenter(options.istanbul);
 
-    global[configs.test_coverage.istanbul.coverageVariable] = {};
+    global[options.coverageVariable] = {};
 
     Module._extensions['.js'] = function (module, filename) {
         var src = fs.readFileSync(filename, {encoding: 'utf8'});
@@ -21,7 +22,7 @@ initIstanbulHookHack = function (options) {
             }
         }
 
-        if (!filename.match(configs.test_coverage.istanbul.skip)) {
+        if (!filename.match(options.istanbul.exclude)) {
             src = instrumenter.instrumentSync(src, filename);
         }
 
@@ -33,17 +34,16 @@ createTask = function (options) {
     return function () {
         var init = initIstanbulHookHack(options.istanbul),
             Collector = istanbul.Collector,
-            mocha = require('gulp-mocha');
 
         return gulp.src(['test/**/*.js', 'test/components/*.js*'])
         .pipe(mocha(options.mocha))
         .on('end', function () {
             var collector = new Collector();
 
-            collector.add(global[configs.test_coverage.istanbul.coverageVariable]);
+            collector.add(global[options.istanbul.coverageVariable]);
 
-            options.istanbulReports.reporters.forEach(function (R) {
-                istanbul.Report.create(R, {dir: configs.test_coverage.istanbul.directory}).writeReport(collector, true);
+            options.coverage.reporters.forEach(function (R) {
+                istanbul.Report.create(R, {dir: configs.coverage.directory}).writeReport(collector, true);
             });
         });
     };
